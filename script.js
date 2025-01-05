02a3d5c79e3c139e2500a9b7f7741ee222b1e7de
Nerdeee <george.larres@gmail.com>
2025-01-04 19:08:40 -0800

    JS higher order function testing

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const testBtn = document.querySelector('.testBtn')
 testBtn.addEventListener("click", (e) => {
     console.log(e.target)
 })
+
+const array = [30, 13, 7, 9, 66, 89, 435]
+//let new_arr = array.forEach((val, idx, arr) => { arr[idx] = val * val })
+const new_arr = array.filter(x => x > 21)
+const sorted_new_arr = new_arr.sort((a, b) => a - b)
+const reduced = sorted_new_arr.reduce((total, val) => total + val, 0)
+console.log('old array - ', array)
+console.log('new array - ', sorted_new_arr)
+console.log('reduced val - ', reduced)
